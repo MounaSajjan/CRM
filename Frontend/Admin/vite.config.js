3a3b7d405f80b09d6b3de5686d35107d47693983
MounaSajjan <mounamc267@gmail.com>
2025-06-30 13:42:52 +0530

    frontend updated

--- Frontend/Admin/vite.config.js
+++ Frontend/Admin/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_API_BASE || '/CRM',
+  base: process.env.VITE_API_BASE || 'CRM/tree/main/Frontend/Admin',
   build: {
     outDir: 'dist', // Vercel auto-detects this
   },
